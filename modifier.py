fe45bb07b7b11a4c46fb5587000710a5a011b02f
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2022-07-13 02:59:14 +0000

    added new conditions

--- modifier.py
+++ modifier.py
@@ -1,8 +1,8 @@
 import os
 
 curr_dir = os.getcwd()
-output_dir = os.path.join(curr_dir,"modified")
-files = os.listdir(os.path.join(curr_dir,'html'))
+output_dir = os.path.join(curr_dir,"new_html")
+files = os.listdir(os.path.join(curr_dir,'new'))
 openString = '''<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,7 +19,7 @@ closingString = '''
 files.sort()
 
 for file in files:
-    with open(os.path.join(curr_dir,'html',file),'r') as fileHandle:
+    with open(os.path.join(curr_dir,'new',file),'r') as fileHandle:
         
         try:
             print("Successful Handling File {}".format(file))
@@ -28,10 +28,12 @@ for file in files:
 
             finalString = openString + data + closingString
 
-            newFile = open(os.path.join(curr_dir,'new_html',file),'w')
+            newFile = open(os.path.join(output_dir,file),'w')
 
             newFile.write(finalString)
 
             newFile.close()
         except:
             print("Unsuccessful {}".format(file))
+        
+        fileHandle.close()
